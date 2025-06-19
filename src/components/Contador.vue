4544eaefe489897ca22698de6b69c48b7ef2dff8
Jesus <jesusfernandoalvarez8@gmail.com>
2025-06-19 17:33:13 -0500

    Crear componentes

---
+++ src/components/Contador.vue
@@ -0,0 +1,30 @@
+
+<template>
+ <H1>Hola mundo</H1>
+<p>{{ contador }}</p>
+<button @click="Incrementar">Incrementar</button>
+<button @click="Decrementar">Decrementar</button>
+
+</template>
+
+<script setup>
+import { ref } from 'vue';  
+const contador = ref(0);
+const Incrementar = () => {
+  contador.value++;
+}
+
+const Decrementar = () => {
+  contador.value--;
+}
+
+</script>
+
+
+<style scoped>
+
+p{
+  color: red;
+}
+
+</style>
